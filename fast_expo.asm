670ff59994738d9a75629b7fcf7fe8ebe51cd14c
Anshik Sahu <97736240+AnshikSahu@users.noreply.github.com>
2023-02-16 06:06:21 +0000

    expo up for testing#

--- fast_expo.asm
+++ fast_expo.asm
@@ -1,7 +1,6 @@
 .global main
 
 main:
-#take two integers as input and store in $a0 and $a1 then call exponentation
     li $v0,5
     syscall
     mv $a0,$v0
@@ -15,29 +14,29 @@ main:
         
 
 exponentation:	
-		addi $sp,$sp,-0x08
-		sw $t0,0x04($sp)
-		sw $s0,0x00($sp)
-		beq $a1,$zero,jumpd
-		andi $t0,$a1,0x01
-		srl $a1,$a1,0x01
-		mv $s0,$a0
-		mul $a0,$a0,$a0
-		addi $sp,$sp,-0x04
-		sw $ra,0x00($sp)
-		jal exponentiation
-		lw $ra,0x00($sp)
-		addi $sp,$sp,0x04
-		beq $t0,$zero,jumpend
-		mul $v0,$v0,$s0
-		j jumpend
+	addi $sp,$sp,-0x08
+	sw $t0,0x04($sp)
+	sw $s0,0x00($sp)
+	beq $a1,$zero,jumpd
+	andi $t0,$a1,0x01
+	srl $a1,$a1,0x01
+	mv $s0,$a0
+    mul $a0,$a0,$a0
+    addi $sp,$sp,-0x04
+    sw $ra,0x00($sp)
+    jal exponentiation
+    lw $ra,0x00($sp)
+    addi $sp,$sp,0x04
+    beq $t0,$zero,jumpend
+    mul $v0,$v0,$s0
+    j jumpend
 
 jumpend:		
-		lw $t0,0x04($sp)
-		lw $s0,0x00($sp)
-		addi $sp,$sp,0x08
-		j $ra
+    lw $t0,0x04($sp)
+    lw $s0,0x00($sp)
+    addi $sp,$sp,0x08
+    j $ra
 
 jumpd:
-		mvi	$v0,0x01
-		j	jumpend
+    mvi	$v0,0x01
+    j	jumpend
