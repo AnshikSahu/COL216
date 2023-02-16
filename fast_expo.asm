eac0bbaaea410bf8d65d571cb24f3dde254ac5b6
Anshik Sahu <97736240+AnshikSahu@users.noreply.github.com>
2023-02-16 10:09:40 +0000

    none

--- fast_expo.asm
+++ fast_expo.asm
@@ -23,14 +23,16 @@ exponentiation:
 	andi $t0,$a1,0x01
 	srl $a1,$a1,0x01
 	add $s0,$a0,$zero
-    mul $a0,$a0,$a0
+    mult $a0,$a0
+    mflo $a0
     addi $sp,$sp,-0x04
     sw $ra,0x00($sp)
     jal exponentiation
     lw $ra,0x00($sp)
     addi $sp,$sp,0x04
     beq $t0,$zero,jumpend
-    mul $v0,$v0,$s0
+    mult $v0,$s0
+    mflo $v0
     j jumpend
 
 jumpend:		
