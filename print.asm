f6bcd3b05848872725a30a24a63c487ad66d5843
SanyaMittal17 <97736276+SanyaMittal17@users.noreply.github.com>
2023-02-17 06:57:01 +0530

    Update print.asm

--- print.asm
+++ print.asm
@@ -1,22 +1,22 @@
 .text
 main:   
-    # read input string from user
-    li $v0, 0x08       # system call code for reading string
-    li $a1, 0100    # allocate 256 bytes for the string
-    syscall         # execute system call
+    # Input taken 
+    li $v0, 0x08       
+    li $a1, 0100    # Creating space for string
+    syscall         
     
-    # save input string to heap
-    move $s0, $v0    # move address of input string to $s0
-    li $v0, 0x09       # system call code for sbrk
-    li $a0, 0100     # allocate 256 bytes for the string
-    syscall         # execute system call
-    sw $s0, 0x00($v0)   # store input string at allocated address
+    # Storing input string to heap
+    move $s0, $v0    # copy address of input string to $s0
+    li $v0, 0x09      
+    li $a0, 0100     # Creating space for string
+    syscall        
+    sw $s0, 0x00($v0)   # store input string at base address i.e. return address
     
-    # print input string
-    li $v0, 0x04       # system call code for printing string
-    move $a0, $s0    # move address of input string to $a0
-    syscall         # execute system call
+    # Printing input string
+    li $v0, 0x04      
+    move $a0, $s0    # copy address of input string to $a0
+    syscall         
     
     # exit program
-    li $v0, 0x0A      # system call code for exiting program
-    syscall         # execute system call
+    li $v0, 0x0A      
+    syscall         
