5f1629d2c6da75200a5779249b5a711bdfe19725
Anshik Sahu <97736240+AnshikSahu@users.noreply.github.com>
2023-02-17 19:06:41 +0000

    done and tested

---
+++ binarysearch.asm
@@ -0,0 +1,109 @@
+# Iterative binary search in MIPS assembly
+
+.data
+arr: .word 0:100                      # array to search
+n: .word 0                            # length of the array
+key: .word 0                          # key to search
+found_msg: .asciiz "Found key: "      # message to print when key is found
+not_found_msg: .asciiz "Key not found" # message to print when key is not found
+input_prompt: .asciiz "Enter the length of the array: " # prompt for array length input
+arr_prompt: .asciiz "Enter the elements of the array: " # prompt for array elements input
+key_prompt: .asciiz "Enter the key to search: " # prompt for user input
+
+.text
+.globl main
+main:
+    # Prompt for array length and read n
+    li $v0, 4
+    la $a0, input_prompt
+    syscall
+
+    li $v0, 5
+    syscall
+    sw $v0, n
+
+    # Prompt for array elements and read arr
+    li $v0, 4
+    la $a0, arr_prompt
+    syscall
+
+    la $t0, arr
+    lw $t1, n
+    li $t2, 0
+
+    loop1:
+        beq $t2, $t1, exit1
+
+        li $v0, 5
+        syscall
+        sw $v0, ($t0)
+
+        addi $t2, $t2, 1
+        addi $t0, $t0, 4
+
+        j loop1
+
+    exit1:
+
+    # Prompt for key and read key
+    li $v0, 4
+    la $a0, key_prompt
+    syscall
+
+    li $v0, 5
+    syscall
+    sw $v0, key
+
+    # Load array address, length, and key
+    la $t0, arr
+    lw $t1, n
+    lw $t2, key
+    li $t3, 0
+    addi $t4, $t1, -1
+
+loop:
+    # Exit loop if left index >= right index
+    ble $t4, $t3, exit2
+
+    # Calculate middle index and load value at middle index
+    add $t5, $t3, $t4
+    srl $t5, $t5, 1
+    sll $t5, $t5, 2
+    add $t5, $t5, $t0
+    lw $t6,0($t5)
+
+    # Compare middle value to key
+    bne $t6, $t2, check
+
+    # Print message and exit if key is found
+    li $v0, 4
+    la $a0, found_msg
+    syscall
+
+    lw $a0, 0($t5)
+    li $v0, 1
+    syscall
+
+    j exit
+
+check:
+    # If key is in left half, search left
+    ble $t3, $t5, left
+    addi $t3, $t5, 1
+    j loop
+
+left:
+    # If key is in right half, search right
+    addi $t4, $t5, -1
+    j loop
+
+exit2:
+    # Print message and exit if key is not found
+    li $v0, 4
+    la $a0, not_found_msg
+    syscall
+    j exit
+
+exit:
+    li $v0, 10
+    syscall
